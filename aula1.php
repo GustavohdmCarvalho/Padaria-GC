0ee311edcb659169e2de667854e3396da3dcdc7b
Gustavo Carvalho <gustavocarvalhoprofissional@gmail.com>
2023-11-16 21:18:47 -0300

    aula 1 de PHP criada

---
+++ aula1.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nome = "Gustavo Carvalho";
+$estado = "São Paulo-SP";
+$idade = "16 anos";
+$cidade = "Ferraz de Vasconcelos-SP";
+$estudante = "Programação Front-end e Back-end";
+
+echo "Meu nome é $nome e moro em $cidade no estado de $estado tenho $idade e sou estudante de $estudante";
+
+?>
+
+<!-- $ é as variaveis -->
+<!-- = significa "recebe" a informação que está entre aspas -->
+<!-- echo ele reproduz como a forma desejada -->
